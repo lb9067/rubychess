776ea8366ff6bbd2c09aee7bf354740ae73f09cd
Louis Burwell <misterburwell@yahoo.com>
2017-10-21 15:38:33 -0400

    added knight class with methods: change_spot and update_potential

---
+++ chess.rb
@@ -0,0 +1,32 @@
+class Knight
+ attr_accessor :spot, :potential
+ def initialize(spot)
+   @spot = spot
+   @potential = []
+   update_potential
+ end
+ 
+ def update_potential
+   x = @spot[0]
+   y = @spot[1]
+   potential = [
+               [x+2,y+1],
+               [x+2,y-1],
+               [x-2,y+1],
+               [x-2,y-1],
+               [x+1,y+2],
+               [x+1,y-2],
+               [x-1,y+2],
+               [x-1,y-2]]
+   potential.each do |a|
+     a.reject! { a.any? { |b| b < 1 || b > 8 }}
+   end
+   potential.delete([])
+   @potential = potential
+ end
+
+ def change_spot(spot)
+   @spot = spot
+   update_potential
+ end
+end

eb2ca71ed435b7ccd4f0e5b487a4ca107a7de9ef
Louis Burwell <misterburwell@yahoo.com>
2017-10-25 11:19:44 -0400

    added rook

--- chess.rb
+++ chess.rb
@@ -1,8 +1,13 @@
+#implement icons properly
+#update pawn classes to white and black
+#add bishop, queen, king
+
+
 class Knight
   attr_accessor :spot, :potential
   attr_reader :icon, :color, :opposite
   def initialize(spot,color="white")
-    color == "white" ? @icon = "\u2658" : @icon = "\u265E"
+    #color == "white" ? @icon = "\u2658" : @icon = "\u265E"
     color == "white" ? @color = "white" : @color = "black"
     color == "white" ? @opposite = "black" : @opposite = "white"
     @spot = spot
@@ -55,6 +60,51 @@ class Pawn < Knight
       a.reject! { a.any? { |b| b < 1 || b > 8 } }
     end
     potential.delete([])
+    @potential = potential
+  end
+end
+
+class Rook < Knight
+
+  def update_potential
+    x = @spot.spot[0]
+    y = @spot.spot[1]
+    potential = []
+    unless x == 8
+      x += 1
+      until x >= 9 || Game.whos_here([x,y]) != " "
+        potential << [x,y]
+        x += 1
+        puts "right added"
+      end
+    end
+    x = @spot.spot[0]
+    unless x == 1
+      x -= 1
+      until x <= 0 || Game.whos_here([x,y]) != " "
+        potential << [x,y]
+        x -= 1
+        puts "left added"
+      end
+    end
+    x = @spot.spot[0]
+    unless y == 8
+      y += 1
+      until y >= 9 || Game.whos_here([x,y]) != " "
+        potential << [x,y]
+        y += 1
+        puts "up added"
+      end
+    end
+    y = @spot.spot[1]
+    unless y == 1
+      y -= 1
+      until y <= 0 || Game.whos_here([x,y]) != " "
+        potential << [x,y]
+        y -= 1
+        puts "down added"
+      end
+    end
     @potential = potential
   end
 end
